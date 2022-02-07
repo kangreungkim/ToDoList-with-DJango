9298666c70f934612513c4d571ad833751cb7d6a
“kangreungkim” <kangreung.kim@gmail.com>
2022-02-08 08:40:57 +0900

    first page

--- ToDoList/my_to_do_app/views.py
+++ ToDoList/my_to_do_app/views.py
@@ -1,3 +1,9 @@
+from typing import Any
 from django.shortcuts import render
+#from http.client import HTTPResponse
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    #return HTTPResponse('my_to_do_app first page')
+    return HttpResponse('my_to_do_app first page')
